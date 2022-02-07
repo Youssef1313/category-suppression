0f129a58631f1aca07a1e9d9f23ab759bd4012b9
Youssef1313 <youssefvictor00@gmail.com>
2022-02-07 09:30:32 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyNamespace
+{
+    // Violates CA1200
+    /// <summary>
+    /// Type <see cref="T:C" /> contains method <see cref="F:C.F" />
+    /// </summary>
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
